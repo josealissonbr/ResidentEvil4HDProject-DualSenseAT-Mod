c88f89a23b5463c0c0261fd3111cf31deacafa29
josealissonbr <98172458+josealissonbr@users.noreply.github.com>
2022-03-29 19:23:02 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ETS2_DualSenseAT_Mod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DynamicTriggers = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rgbledHealth = new System.Windows.Forms.CheckBox();
+            this.ammoAdpTriggers = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(12, 165);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(89, 13);
+            this.statusLbl.TabIndex = 0;
+            this.statusLbl.Text = "Status: Unknown";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ResidentEvil4HDProject.Properties.Resources.resident_evil_4_hd_project_trailer_lancamento3;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DynamicTriggers
+            // 
+            this.DynamicTriggers.DoWork += new System.ComponentModel.DoWorkEventHandler(this.DynamicTriggers_DoWork);
+            this.DynamicTriggers.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.DynamicTriggers_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ammoAdpTriggers);
+            this.groupBox1.Controls.Add(this.rgbledHealth);
+            this.groupBox1.Location = new System.Drawing.Point(13, 182);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 121);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DualSense Settings";
+            // 
+            // rgbledHealth
+            // 
+            this.rgbledHealth.AutoSize = true;
+            this.rgbledHealth.Checked = true;
+            this.rgbledHealth.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.rgbledHealth.Location = new System.Drawing.Point(7, 20);
+            this.rgbledHealth.Name = "rgbledHealth";
+            this.rgbledHealth.Size = new System.Drawing.Size(132, 17);
+            this.rgbledHealth.TabIndex = 0;
+            this.rgbledHealth.Text = "RGBLed Health Effect";
+            this.rgbledHealth.UseVisualStyleBackColor = true;
+            // 
+            // ammoAdpTriggers
+            // 
+            this.ammoAdpTriggers.AutoSize = true;
+            this.ammoAdpTriggers.Checked = true;
+            this.ammoAdpTriggers.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ammoAdpTriggers.Location = new System.Drawing.Point(7, 43);
+            this.ammoAdpTriggers.Name = "ammoAdpTriggers";
+            this.ammoAdpTriggers.Size = new System.Drawing.Size(150, 17);
+            this.ammoAdpTriggers.TabIndex = 1;
+            this.ammoAdpTriggers.Text = "Ammo Adaptative Triggers";
+            this.ammoAdpTriggers.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 315);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusLbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(342, 354);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(342, 223);
+            this.Name = "Form1";
+            this.Text = "Resident Evil 4 HD Project| DualSense AT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker DynamicTriggers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox rgbledHealth;
+        private System.Windows.Forms.CheckBox ammoAdpTriggers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,338 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Shared;
+using System.Threading;
+using Memory;
+
+namespace ETS2_DualSenseAT_Mod
+{
+    public partial class Form1 : Form
+    {
+
+        static UdpClient client;
+        static IPEndPoint endPoint;
+
+        static bool Server_Initialized = false;
+
+        Mem meme = new Mem();
+        static bool Connect()
+        {
+            try
+            {
+                client = new UdpClient();
+                var portNumber = File.ReadAllText(@"C:\Temp\DualSenseX\DualSenseX_PortNumber.txt");
+                endPoint = new IPEndPoint(Triggers.localhost, Convert.ToInt32(portNumber));
+                Server_Initialized = true;
+                return true;
+            }catch(Exception ex)
+            {
+                Server_Initialized = false;
+                return false;
+            }
+        }
+
+        static void Send(Packet data)
+        {
+            if (!Server_Initialized)
+                return;
+            var RequestData = Encoding.ASCII.GetBytes(Triggers.PacketToJson(data));
+            client.Send(RequestData, RequestData.Length, endPoint);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            statusLbl.Text = "Status: Ready!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int PID = meme.GetProcIdFromName("bio4");
+
+            if (PID > 0)
+            {
+                if (!Connect())
+                {
+                    MessageBox.Show("Failed to connect to the DSX UDP Server (" + Triggers.localhost, Convert.ToInt32(File.ReadAllText(@"C:\Temp\DualSenseX\DualSenseX_PortNumber.txt")) + ")");
+                    Application.Exit();
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Resident Evil 4 isn't running, please open game first!", "DualSense AT Mod");
+                Application.Exit();
+            }
+        }
+
+        static int iStep = 0;
+        static int iMaxSteps = 0;
+        private void InitializationEffect()
+        {
+
+            if (iMaxSteps < 5){
+                Packet p = new Packet();
+
+                int controllerIndex = 0;
+                p.instructions = new Instruction[4];
+
+                if (iStep == 0)
+                {
+                    p.instructions[0].type = InstructionType.RGBUpdate;
+                    p.instructions[0].parameters = new object[] { controllerIndex, 237, 61, 7 };
+                    
+                    // PLAYER LED 1-5 true/false state
+                    p.instructions[1].type = InstructionType.PlayerLED;
+                    p.instructions[1].parameters = new object[] { controllerIndex, true, false, false, false, false };
+                    
+                    iStep = 1;
+                }
+                else if (iStep == 1)
+                {
+                    p.instructions[0].type = InstructionType.RGBUpdate;
+                    p.instructions[0].parameters = new object[] { controllerIndex, 252, 0, 0 };
+                    
+                    // PLAYER LED 1-5 true/false state
+                    p.instructions[1].type = InstructionType.PlayerLED;
+                    p.instructions[1].parameters = new object[] { controllerIndex, false, true, false, false, false };
+                    
+                    iStep = 2;
+                }
+                else if (iStep == 2)
+                {
+                    p.instructions[0].type = InstructionType.RGBUpdate;
+                    p.instructions[0].parameters = new object[] { controllerIndex, 148, 22, 0 };
+                    
+                    // PLAYER LED 1-5 true/false state
+                    p.instructions[1].type = InstructionType.PlayerLED;
+                    p.instructions[1].parameters = new object[] { controllerIndex, false, false, true, false, false };
+                    
+                    iStep = 3;
+                }
+                else if (iStep == 3)
+                {
+                    p.instructions[0].type = InstructionType.RGBUpdate;
+                    p.instructions[0].parameters = new object[] { controllerIndex, 237, 61, 7 };
+                   
+                    // PLAYER LED 1-5 true/false state
+                    p.instructions[1].type = InstructionType.PlayerLED;
+                    p.instructions[1].parameters = new object[] { controllerIndex, false, false, false, true, false };
+                    
+
+                    iStep = 4;
+                }
+                else if (iStep == 4)
+                {
+                    p.instructions[0].type = InstructionType.RGBUpdate;
+                    p.instructions[0].parameters = new object[] { controllerIndex, 148, 22, 0 };
+                    
+                    // PLAYER LED 1-5 true/false state
+                    p.instructions[1].type = InstructionType.PlayerLED;
+                    p.instructions[1].parameters = new object[] { controllerIndex, false, false, false, false, true };
+                    
+
+                    iStep = 0;
+                    iMaxSteps += +1;
+                }
+
+                
+
+                Send(p);
+            }
+            else
+            {
+                Packet p = new Packet();
+
+                int controllerIndex = 0;
+                p.instructions = new Instruction[4];
+
+                p.instructions[0].type = InstructionType.RGBUpdate;
+                p.instructions[0].parameters = new object[] { controllerIndex, 119, 3, 252 };
+
+                // PLAYER LED 1-5 true/false state
+                p.instructions[1].type = InstructionType.PlayerLED;
+                p.instructions[1].parameters = new object[] { controllerIndex, true, false, false, false, false };
+
+                Send(p);
+
+                timer1.Enabled = false;
+                DynamicTriggers.RunWorkerAsync();
+            }
+
+        }
+
+        private void gameStaticTriggerValues()
+        {
+
+            Packet p = new Packet();
+
+            int controllerIndex = 0;
+            p.instructions = new Instruction[4];
+
+            p.instructions[0].type = InstructionType.TriggerUpdate;
+            p.instructions[0].parameters = new object[] { controllerIndex, Trigger.Right, TriggerMode.CustomTriggerValue, CustomTriggerValueMode.VibratePulseAB, 61, 255, 255, 5, 67, 119, 0 };
+
+            p.instructions[1].type = InstructionType.TriggerUpdate;
+            p.instructions[1].parameters = new object[] { controllerIndex, Trigger.Left, TriggerMode.Bow, 0, 1, 8, 8};
+
+            Send(p);
+
+        }
+
+        private void gameDynamicTriggers()
+        {
+            Mem dynaMemory = new Mem();
+            bool processOpen = dynaMemory.OpenProcess("bio4");
+            if (processOpen)
+            {
+                int iHeath = dynaMemory.Read2Byte("bio4.exe+85F714");
+                int iAmmo = dynaMemory.Read2Byte("bio4.exe+85F714");
+
+                Packet p = new Packet();
+
+                int controllerIndex = 0;
+                int inst_index = 0;
+
+                p.instructions = new Instruction[4];
+
+                //RGB Led Health Tracker
+                if (rgbledHealth.Checked)
+                {
+                    if (iHeath >= 800)
+                    {
+                        p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                        p.instructions[inst_index].parameters = new object[] { controllerIndex, 0, 255, 0 };
+                        inst_index++;
+                    }
+                    else if (iHeath >= 400)
+                    {
+                        p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                        p.instructions[inst_index].parameters = new object[] { controllerIndex, 255, 187, 0 };
+                        inst_index++;
+                    }
+                    else if (iHeath >= 1)
+                    {
+                        p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                        p.instructions[inst_index].parameters = new object[] { controllerIndex, 255, 0, 0 };
+                        inst_index++;
+                    }
+
+                    else if (iHeath == 0)
+                    {
+
+                        int counter = 0;
+                        while (counter < 3)
+                        {
+
+                            if (counter == 0)
+                            {
+                                p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                                p.instructions[inst_index].parameters = new object[] { controllerIndex, 250, 75, 75 };
+                                inst_index++;
+                            }
+                            else if (counter == 1)
+                            {
+                                p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                                p.instructions[inst_index].parameters = new object[] { controllerIndex, 156, 6, 6 };
+                                inst_index++;
+                            }
+                            else if (counter == 2)
+                            {
+                                p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                                p.instructions[inst_index].parameters = new object[] { controllerIndex, 159, 13, 13 };
+                                inst_index++;
+                            }
+                            else if (counter == 3)
+                            {
+                                p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                                p.instructions[inst_index].parameters = new object[] { controllerIndex, 222, 113, 113 };
+                                inst_index++;
+                            }
+                            else if (counter == 4)
+                            {
+                                p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                                p.instructions[inst_index].parameters = new object[] { controllerIndex, 181, 27, 27 };
+                                inst_index++;
+                            }
+                            else if (counter == 5)
+                            {
+                                p.instructions[inst_index].type = InstructionType.RGBUpdate;
+                                p.instructions[inst_index].parameters = new object[] { controllerIndex, 64, 0, 0 };
+                                inst_index++;
+                            }
+                            Send(p);
+                            counter++;
+                            Thread.Sleep(200);
+                        }
+
+
+                    }
+                }
+                
+                if (ammoAdpTriggers.Checked)
+                {
+
+                }
+
+
+                Send(p);
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Packet p = new Packet();
+
+            int controllerIndex = 0;
+
+            p.instructions = new Instruction[4];
+
+            p.instructions[0].type = InstructionType.TriggerUpdate;
+            p.instructions[0].parameters = new object[] { controllerIndex, Trigger.Right, TriggerMode.Normal };
+
+
+            p.instructions[1].type = InstructionType.TriggerUpdate;
+            p.instructions[1].parameters = new object[] { controllerIndex, Trigger.Left, TriggerMode.Normal };
+
+
+            p.instructions[2].type = InstructionType.RGBUpdate;
+            p.instructions[2].parameters = new object[] { controllerIndex, 66, 135, 245 };
+
+            Send(p);
+            statusLbl.Text = "Status: Closing";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            InitializationEffect();
+        }
+
+        private void DynamicTriggers_DoWork(object sender, DoWorkEventArgs e)
+        {
+            gameDynamicTriggers();
+        }
+
+        private void DynamicTriggers_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            DynamicTriggers.RunWorkerAsync();
+        }
+    }
+}
